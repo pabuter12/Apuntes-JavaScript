420fdad96ea55763a9a84a3bd0e401a303bb1d27
Juan Pablo Arcila <pabloarcila517@gmail.com>
2023-06-17 19:59:21 -0500

    Más ejercicios realizados de clase 1

--- CLASES/clase 1/practica_clase_off/scripts.js
+++ CLASES/clase 1/practica_clase_off/scripts.js
@@ -7,7 +7,7 @@ console.log(typeof variable1);
 console.log(typeof variable2);
 console.log(typeof variable3);
 console.log( typeof variable4); //Para saber que tipo de dato tiene otro objeto hay que utilizar otro OPERADOR.
-
+/*
 //Ejercicio 1:
 
 let nombreUsuario = prompt("Ingrese su nombre");
@@ -32,6 +32,45 @@ const proteina = 1.2;
 let proteinaPorPeso = peso * proteina;
 
 alert("La cantidad de gramos de proteina que debe consumir por cada kilogramo de peso corporal es de" + " " + proteinaPorPeso + " " + "gr")
+
+// Ejercicio 4
+
+let cantidadMascotas = prompt("Ingrese la cantidad de mascotas que tienes");
+
+alert("La cantidad de mascotas que usted tiene es de" + " " + cantidadMascotas);
+
+// Ejercicio 5
+
+let numero1 = 54;
+let numero2 = 24;
+
+let sumaNumeros = numero1 + numero2; //78
+let restaNumeros = numero1 - numero2; // 30
+let multiNumeros = numero1 * numero2; // 1296
+let dividirNumeros = numero1 / numero2; // 2.25
+
+console.log("La suma de los números es de" + " " + sumaNumeros);
+console.log("La resta de los numeros es de " + restaNumeros);
+console.log("La multiplicación de los números es de" + " " + multiNumeros);
+console.log("La división de los números es de" + " " + dividirNumeros);
+
+*/
+// Ejercicio 6
+
+let precioProducto = parseInt(prompt("Ingrese el precio del producto"));
+
+const iva = 1.19;
+
+let precioProductoIva = (precioProducto * iva)
+
+alert("El precio del producto con el iva incluido es de" + " " + precioProductoIva)
+
+console.log(precioProducto)
+
+const numeroX = 2;
+
+console.log(typeof precioProducto);
+console.log(typeof numeroX);
 
 
 
