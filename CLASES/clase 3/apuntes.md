2c1f9708f0f054000fe848281b578840a7e8f527
pabuter12 <pabloarcila517@gmail.com>
2023-06-29 21:53:29 -0500

    Practica de la clase 3, bucles

--- CLASES/clase 3/apuntes.md
+++ CLASES/clase 3/apuntes.md
@@ -72,6 +72,43 @@ for(let i=0; i<10; i++){
     console.log(suma);
 }
 ```
+```javascript
+//Este código calculará la suma de todos los números pares desde 1 hasta el número ingresado por el usuario y mostrará el resultado en la consola.
+let numeroLimite = prompt("Ingrese un número:");
+let suma = 0;
+
+for (let i = 1; i <=numeroLimite; i++){
+    if (i % 2 === 0){ //recordar aqui que el operador de modulo: "%" es para obtener el residuo de una división
+        suma = suma + i;
+    }
+}
+console.log("La suma de los números pares hasta " + numeroLimite + " es: " + suma);
+```
+
+```javascript
+//Imprimir una secuencia de caracteres
+let texto = prompt("Ingrese un texto");
+
+for(let i=0; i < texto.length ; i++){
+    console.log("Caracter numero " + i+ "= " + texto[i]);
+}
+```
+
+```javascript
+//Contar el numero de vocales de una palabra
+
+let palabra = prompt("Ingrese una palabra");
+let contador = 0;
+
+for(let i=0; i < palabra.length ; i++){
+    if ( palabra[i] == "a" || palabra[i] == "e" || palabra[i] == "i" || palabra[i] == "o" || palabra[i] == "u"){
+        contador = contador + 1;
+}
+}
+console.log("La cantidad de vocales en la palabra: " + palabra + " es de " + contador);
+```
+
+
 
 # Sentencia Break
 
@@ -109,6 +146,23 @@ if(fallidos == 3){
 }
 ```
 
+```javascript
+//Busquedad de un caracter en una cadena 
+
+let texto = prompt("Ingrese una cadena de texto");
+let caracter = prompt("Ingrese un caracter a buscar");
+
+for ( var i = 1 ; i < texto.length ; i++){
+    if ( texto[i] == caracter){
+    console.log("Se encontro el caracter indicado: " +"(" + caracter + ") " + "en la letra numero " + i + " del texto: " + texto);
+    break;
+    }
+}
+if( i == texto.length){
+    console.log("El caracter indicado: " + caracter + " no se encontro en el texto indicado: " + texto);
+}
+```
+
 # Sentencia continue
 A veces, cuando escribimos una estructura for, necesitamos que bajo cierta condición, el ciclo saltee esa repetición y siga con la próxima. Para eso se utiliza la sentencia continue. (NO ES TAN COMÚN)
 
@@ -134,8 +188,6 @@ while (entrada != "ESC"){
     //Volvemos a solicitar un dato. En la proxima se evaluar si no es ESC
     entrada = prompt("Ingresar otro dato");
 }
-
-
 ```
 
 ```javascript
@@ -156,6 +208,33 @@ while (edad != "0"){
 }
 ```
 
+```javascript
+// Conteo de vocales en una cadena
+
+let texto = prompt("Ingrese una cadena de texto");
+let contadora = 0;
+let i = 0;
+
+while( i <= texto.length){
+    let caracter = texto[i];
+
+    if( texto[i] == "a" || texto[i] == "e" || texto[i] == "i" || texto[i] == "o" || texto[i] == "u"){
+        contadora = contadora + 1;
+    }
+    i++; //no olvidar incremntar el valor de i, sino entrara en un bucle infinito
+}
+
+console.log("La cantidad de vocales en la cadena de texto es de " + contadora)
+```
+```javascript
+//Contador descendente
+let contador = 10;
+
+while ( contador >= 1){
+    console.log(contador);
+    contador--;
+}
+```
 # DO WHILE
 La estructura do...while permite crear bucles que se ejecutan una o más veces, dependiendo de la condición indicada.
 A diferencia de while, garantiza que el bloque de código se interpreta al menos una vez, porque la condición se evalúa al final.
@@ -199,6 +278,25 @@ do {
 }   while (z<10)
 ```
 Nota: Se puede cambiar el valor de la variable y su limite para ir jugando y entendiendo. 
+
+```javascript
+// Adivinar un numero entro el 1 y el 10. Ejemplo bueno
+
+let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+
+let numeroUsuario;
+
+do{
+    numeroUsuario = parseInt(prompt("Adivine el numero (entre el 1 y el 10)"));
+    if (numeroUsuario < numeroAleatorio){
+        console.log("El nummero a adivinar en mayor. Intente otra vez");
+    }else if(numeroUsuario > numeroAleatorio){
+        console.log("El numero a adivinar es menor. Intente nuevamente");
+    }
+}while (numeroUsuario != numeroAleatorio);
+
+console.log("Felicitaciones! Adivinaste el numero.");
+```
 # Switch
 No es un ciclo. Cuando tengo mucho para evaluar. En vez de poner el if, if else y else. Es una estructura más amigable. Es un CONDICIONAL. 
 Nota: Si no se pone el break se ejecuta todo... Puede haber situacioens que cuando se cumpla una condición tambien se cumplan otras...
--- CLASES/clase 3/practica_clase_off/main_clase3.js
+++ CLASES/clase 3/practica_clase_off/main_clase3.js
@@ -52,31 +52,171 @@
 
 // Switch
 
-let diadelasemana = parseInt(prompt("Ingrese dia de la semana con números"));
+// let diadelasemana = parseInt(prompt("Ingrese dia de la semana con números"));
 
-switch (diadelasemana){
-    case 1:
-        console.log("Lunes");
-        break;
-    case 2:
-        console.log("Martes");
-        break;
-    case 3:
-        console.log("Miercoles");
-        break;
-    case 4:
-        console.log("Jueves");
-        break;
-    case 5:
-        console.log("Viernes");
-        break;
-    case 6:
-        console.log("Sabado");
-        break;
-    case 7:
-        console.log("Domingo");
-        break;
-    default:
-        console.log("No equivale a ningun dia de la semana");
-}
+// switch (diadelasemana){
+//     case 1:
+//         console.log("Lunes");
+//         break;
+//     case 2:
+//         console.log("Martes");
+//         break;
+//     case 3:
+//         console.log("Miercoles");
+//         break;
+//     case 4:
+//         console.log("Jueves");
+//         break;
+//     case 5:
+//         console.log("Viernes");
+//         break;
+//     case 6:
+//         console.log("Sabado");
+//         break;
+//     case 7:
+//         console.log("Domingo");
+//         break;
+//     default:
+//         console.log("No equivale a ningun dia de la semana");
+// }
 
+
+// //Este código calculará la suma de todos los números pares desde 1 hasta el número ingresado por el usuario y mostrará el resultado en la consola.
+// let numeroLimite = prompt("Ingrese un número:");
+// let suma = 0;
+
+// for (let i = 1; i <=numeroLimite; i++){
+//     if (i % 2 === 0){ //recordar aqui que el operador de modulo: "%" es para obtener el residuo de una división
+//         suma = suma + i;
+//     }
+// }
+
+// console.log("La suma de los números pares hasta " + numeroLimite + " es: " + suma);
+
+
+
+// //Codigo que imprime los numeros pares que hay de 1 a 100:
+// for(let i = 1; i<=100 ; i++){
+//     if ( i % 2 == 0){
+//         console.log("El numero " + i + " es par");
+//     }
+// }
+
+
+// //Codigo que calcula la suma de los números impares de 1 hasta 50.
+// let suma = 0;
+
+// for(let i = 1; i <= 50; i++){
+//     if(i % 2 != 0){
+//         suma = suma + i;
+//         console.log("Numero impar: " + i+ "." + " La suma es "+ suma);
+//     }
+// }
+
+
+
+// //Imprimir una secuencia de caracteres
+// let texto = prompt("Ingrese un texto");
+
+// for(let i=0; i < texto.length ; i++){
+//     console.log("Caracter numero " + i+ "= " + texto[i]);
+// }
+
+
+
+// //Contar el numero de vocales de una palabra
+// let palabra = prompt("Ingrese una palabra");
+// let contador = 0;
+
+// for(let i=0; i < palabra.length ; i++){
+//     if ( palabra[i] == "a" || palabra[i] == "e" || palabra[i] == "i" || palabra[i] == "o" || palabra[i] == "u"){
+//         contador = contador + 1;
+// }
+// }
+// console.log("La cantidad de vocales en la palabra: " + palabra + " es de " + contador);
+
+
+// //Busquedad de un caracter en una cadena 
+
+// let texto = prompt("Ingrese una cadena de texto");
+// let caracter = prompt("Ingrese un caracter a buscar");
+
+// for ( var i = 1 ; i < texto.length ; i++){
+//     if ( texto[i] == caracter){
+//     console.log("Se encontro el caracter indicado: " +"(" + caracter + ") " + "en la letra numero " + i + " del texto: " + texto);
+//     break;
+//     }
+// }
+// if( i == texto.length){
+//     console.log("El caracter indicado: " + caracter + " no se encontro en el texto indicado: " + texto);
+// }
+
+
+// //Contador ascendente
+
+// let contador = 1;
+// while ( contador <= 10){
+//     console.log(contador);
+//     contador++;
+// }
+
+
+// // Suma numeros
+// let suma = 0;
+// let numero = parseInt(prompt("Ingrese un numero para sumar"));
+
+// while(numero !== 0){
+//     suma = suma + numero;
+//     numero = parseInt(prompt("Ingrese un otro numero"));
+
+//     if (numero === "0"){
+//         break;
+//     }
+// }
+// console.log("La suma total es " + suma)
+
+
+// //Contador descendente
+// let contador = 10;
+
+// while ( contador >= 1){
+//     console.log(contador);
+//     contador--;
+// }
+
+
+// // Conteo de vocales en una cadena
+
+// let texto = prompt("Ingrese una cadena de texto");
+// let contadora = 0;
+// let i = 0;
+
+// while( i <= texto.length){
+//     let caracter = texto[i];
+
+//     if( texto[i] == "a" || texto[i] == "e" || texto[i] == "i" || texto[i] == "o" || texto[i] == "u"){
+//         contadora = contadora + 1;
+//     }
+//     i++; //no olvidar incremntar el valor de i, sino entrara en un bucle infinito
+// }
+
+// console.log("La cantidad de vocales en la cadena de texto es de " + contadora)
+
+
+
+// // Adivinar un numero
+
+// let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+
+// let numeroUsuario;
+
+// do{
+//     numeroUsuario = parseInt(prompt("Adivine el numero (entre el 1 y el 10)"));
+//     if (numeroUsuario < numeroAleatorio){
+//         console.log("El nummero a adivinar en mayor. Intente otra vez");
+//     }else if(numeroUsuario > numeroAleatorio){
+//         console.log("El numero a adivinar es menor. Intente nuevamente");
+//     }
+// }while (numeroUsuario != numeroAleatorio);
+
+// console.log("Felicitaciones! Adivinaste el numero.");
